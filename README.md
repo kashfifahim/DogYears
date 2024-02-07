0b27d570d96095be65942ed145c1d407d747d712
Kashfi Fahim <kashfi.fahim@gmail.com>
2024-02-07 01:49:21 -0500

    completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DogYears
 Convert your age from “human years” to “dog years
+
+## How To Run The Script
+Run node and script with your first name and your age:
+```Computer-Command-Line % node app.js YOUR_NAME 35```
---
+++ app.js
@@ -0,0 +1,15 @@
+function dogYears(myName, myAge) {
+    myName = myName.toLowerCase();
+    let earlyYears = 2;
+    earlyYears *= 10.5;
+    let laterYears = myAge - 2;
+    laterYears *= 4;
+    let myAgeInDogYears = earlyYears + laterYears;
+    console.log(`My name is ${myName}.
+I am ${myAge} years old in human years 
+which is ${myAgeInDogYears} years old in dog yeras.`)
+}
+
+let myName = process.argv[2];
+let myAge = process.argv[3];
+dogYears(myName, myAge);
